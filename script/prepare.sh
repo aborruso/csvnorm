049fd20175b1e089f89dbcf46e380bafb612fdfc
aborruso <aborruso@gmail.com>
2025-01-24 17:30:15 +0100

    feat: Add tmp folder and output CSV files to script/prepare.sh

--- script/prepare.sh
+++ script/prepare.sh
@@ -7,4 +7,8 @@ set -o pipefail
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-duckdb -c "copy (from read_csv('../test/Trasporto Pubblico Locale Settore Pubblico Allargato - Indicatore 2000-2020 Trasferimenti Correnti su Entrate Correnti.csv',store_rejects = true,sample_size=-1)) TO '/dev/null';copy (FROM reject_errors) to 'reject_errors.csv'"
+mkdir -p "${folder}"/tmp
+
+duckdb -c "copy (from read_csv('../test/Trasporto Pubblico Locale Settore Pubblico Allargato - Indicatore 2000-2020 Trasferimenti Correnti su Entrate Correnti.csv',store_rejects = true,sample_size=-1)) TO '/dev/null';copy (FROM reject_errors) to '${folder}/tmp/reject_errors.csv'"
+
+duckdb --csv -c "select * from read_csv('../test/Trasporto Pubblico Locale Settore Pubblico Allargato - Indicatore 2000-2020 Trasferimenti Correnti su Entrate Correnti.csv',sample_size=-1)" >"${folder}/tmp/Trasporto Pubblico Locale Settore Pubblico Allargato - Indicatore 2000-2020 Trasferimenti Correnti su Entrate Correnti.csv"
